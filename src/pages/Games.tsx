66142828f185d7b19ef6ec794c193af96ea832c2
duade1 <161097468+duade1@users.noreply.github.com>
2026-03-17 17:28:05 +0900

    Update Games.tsx

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,9 +1,16 @@
+import {useState} from 'react'
+import { Button } from '../components/Button';
+
 export const Games = () => {
+  const [Color, setColor] = useState<"primary" | "secondary">("primary");
+
+  const toggle = () => {
+    setColor((prev) => (prev === "primary" ? "secondary" : "primary"));
+  };
+
   return (
     <div className="bg-blue-200 flex justify-center items-center relative flex-col">
-      <p className="font-serif">
-        игры
-      </p>
+      <Button size="large" color={Color} title="Игры" onClick={toggle}></Button>
     </div>
   );
 };
